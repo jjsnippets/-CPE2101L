fd8416eb5a2256310903576b81b2687b277fdabf
jjsnippets <jjsnippets@gmail.com>
2024-09-12 09:00:47 +0800

    before earthquake drill

---
+++ vendingMachineAct/MainExec.java
@@ -0,0 +1,64 @@
+package vendingMachineAct;
+
+import java.util.Scanner;
+ 
+
+public class MainExec {
+
+	public static void main(String[] args) {
+		final int MAX_OBJECTS = 30;
+		
+		Machine machines[] = new Machine[MAX_OBJECTS];
+		Owner owners[]= new Owner[MAX_OBJECTS];
+		Technician technicians[] = new Technician[MAX_OBJECTS];
+		
+		Customer johnDoe = new Customer();
+		int mode;
+		
+		do {
+			mode = screens();
+			
+			switch (mode) {
+				case 1:
+					
+					break;
+					
+				case 2:
+					
+					break;
+				
+				case 3:
+					
+					break;
+					
+				case 4
+			
+			
+			}
+			
+			
+			
+		} while (mode != 4);
+		
+		
+
+	}
+	
+	// initial screen / change modes screen
+	public static int screens() {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Run as:");
+		System.out.println("[1] Customer");
+		System.out.println("[2] Technician");
+		System.out.println("[3] Owner");
+		System.out.println("[4] Terminate program");
+		System.out.print(" >> ");
+		
+		int x = input.nextInt();
+		input.close();
+		
+		return x;
+	}
+
+}
