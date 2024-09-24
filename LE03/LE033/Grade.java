16fb720ac116c32501eeae035f9d06c445142cea
jjsnippets <jjsnippets@gmail.com>
2024-09-24 10:12:52 +0800

    re-factored code

--- LE03/LE033/Grade.java
+++ LE03/LE033/Grade.java
@@ -1,43 +1,66 @@
 package LE03.LE033;
 
 public class Grade {
-	@SuppressWarnings("unused")
 	// class attributes
 	private int A, B, C, D, F;
 	private int total;
-	private StringBuilder Ax, Bx, Cx, Dx, Fx;
 	
 	// class constructor
-	public Grade(int A, int B, int C, int D, int F) {
+	public Grade(int a, int b, int c, int d, int f) {
+		this.A = a;
+		this.B = b;
+		this.C = c;
+		this.D = d;
+		this.F = f;
+		
+		// calculates total grades
+		this.total = a + b + c + d + f;
+	}
+	
+	// accessors and mutators
+	public int getA() { return this.A; }
+	public void setA(int a) { this.A = a; }
+	
+	public int getB() { return this.B; }
+	public void setB(int b) { this.B = b; }
+	
+	public int getC() { return this.C; }
+	public void setC(int c) { this.C = c; }
+	
+	public int getD() { return this.D; }
+	public void setD(int d) { this.D = d; }
+	
+	public int getF() { return this.F; }
+	public void setF(int f) { this.F = f; }
+	
+	
+	// method for printing the data of the object in a bar graph format
+	public void printGraph() {
 		// declarations
 		int As, Bs, Cs, Ds, Fs;
 		
-		this.A = A;
-		this.B = B;
-		this.C = C;
-		this.D = D;
-		this.F = F;
-		
-		// calculates total grades
+		// re-calculates total grades
 		this.total = A + B + C + D + F;
 		
 		// calculates fraction of the 50 a grade gets
-		As = (int) Math.round(50.0 * A / this.total);
-		Bs = (int) Math.round(50.0 * B / this.total);
-		Cs = (int) Math.round(50.0 * C / this.total);
-		Ds = (int) Math.round(50.0 * D / this.total);
-		Fs = (int) Math.round(50.0 * F / this.total);
+		As = (int) Math.round(50.0 * this.A / this.total);
+		Bs = (int) Math.round(50.0 * this.B / this.total);
+		Cs = (int) Math.round(50.0 * this.C / this.total);
+		Ds = (int) Math.round(50.0 * this.D / this.total);
+		Fs = (int) Math.round(50.0 * this.F / this.total);
 		
-		// equivalent bar graph of a grade as StringBuilder
-		this.Ax = Grade.fillX(As, 'A');
-		this.Bx = Grade.fillX(Bs, 'B');
-		this.Cx = Grade.fillX(Cs, 'C');
-		this.Dx = Grade.fillX(Ds, 'D');
-		this.Fx = Grade.fillX(Fs, 'F');
+		System.out.println("0    10   20   30   40   50   60   70   80   90  100%");
+		System.out.println("|    |    |    |    |    |    |    |    |    |   |");
+		System.out.println("**************************************************");
+		System.out.println(Grade.fillX(As, 'A'));
+		System.out.println(Grade.fillX(Bs, 'B'));
+		System.out.println(Grade.fillX(Cs, 'C'));
+		System.out.println(Grade.fillX(Ds, 'D'));
+		System.out.println(Grade.fillX(Fs, 'F'));
 	}
 	
 	// method for making a single bar in the bar graph
-	public static StringBuilder fillX(int x, char l) {
+	private static StringBuilder fillX(int x, char l) {
 		StringBuilder bar = new StringBuilder();
 		
 		for (int i = 0; i < x; i++) {
@@ -50,17 +73,4 @@ public class Grade {
 		
 		return bar;
 	}
-	
-	// method for printing the data of the object in a bar graph format
-	public void printGraph() {
-		System.out.println("0    10   20   30   40   50   60   70   80   90  100%");
-		System.out.println("|    |    |    |    |    |    |    |    |    |   |");
-		System.out.println("**************************************************");
-		System.out.println(this.Ax);
-		System.out.println(this.Bx);
-		System.out.println(this.Cx);
-		System.out.println(this.Dx);
-		System.out.println(this.Fx);
-	}
-	
 }
